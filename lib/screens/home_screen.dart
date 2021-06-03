1f3ce3130eaef252b507a7ecdf4878dac10c9ad6
Theshan Kaveen <64361534+tkaveen@users.noreply.github.com>
2021-06-03 19:18:24 +0530

    Homapage updated

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -11,8 +12,36 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       body: SafeArea(
         child: Container(
-          child:
-              ListView(physics: BouncingScrollPhysics(), children: <Widget>[]),
+          child: ListView(physics: BouncingScrollPhysics(), children: <Widget>[
+            Container(
+              height: 57.6,
+              margin: EdgeInsets.only(top: 28, left: 28.8, right: 28.8),
+              child: Row(
+                children: <Widget>[
+                  Container(
+                    height: 57.6,
+                    width: 57.6,
+                    padding: EdgeInsets.all(18),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(9.6),
+                      color: Color(0x080a0928),
+                    ),
+                    child: SvgPicture.asset('assets/svg/icon_drawer.svg '),
+                  ),
+                  Container(
+                    height: 57.6,
+                    width: 57.6,
+                    padding: EdgeInsets.all(18),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(9.6),
+                      color: Color(0x080a0928),
+                    ),
+                    child: SvgPicture.asset('assets/svg/icon_search.svg '),
+                  ),
+                ],
+              ),
+            )
+          ]),
         ),
       ),
     );
