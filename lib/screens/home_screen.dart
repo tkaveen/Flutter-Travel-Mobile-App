60855d27b36601bf6ca3fe0d6d8a0d12963a22cc
Theshan Kaveen <64361534+tkaveen@users.noreply.github.com>
2021-06-05 19:13:43 +0530

    add new widget and homepage updated

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,8 +1,12 @@
 // import 'dart:html';
+
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
 // import 'package:flutter_svg/flutter_svg.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+// import 'package:tab_indicator_styler/tab_indicator_styler.dart';
+import 'package:travel_app/widgets/custom_tab_indicator.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -61,6 +65,53 @@ class _HomeScreenState extends State<HomeScreen> {
                     fontFamily: 'Jost',
                     fontSize: 50,
                     fontWeight: FontWeight.w700),
+              ),
+            ),
+            Container(
+              height: 30,
+              margin: EdgeInsets.only(left: 14.4, top: 28.8),
+              child: DefaultTabController(
+                length: 4,
+                child: TabBar(
+                  labelPadding: EdgeInsets.only(left: 14.4, right: 14.4),
+                  indicatorPadding: EdgeInsets.only(left: 14.4, right: 14.4),
+                  isScrollable: true,
+                  labelColor: Color(0xFF000000),
+                  unselectedLabelColor: Color(0xFF8a8a8a),
+                  labelStyle: TextStyle(
+                      fontFamily: 'Apercu Pro',
+                      fontSize: 14,
+                      fontWeight: FontWeight.w700),
+                  unselectedLabelStyle: TextStyle(
+                      fontFamily: 'Apercu Pro',
+                      fontSize: 14,
+                      fontWeight: FontWeight.w700),
+                  indicator: RoundedRectangleTabIndicator(
+                      color: Color(0xFF000000), weight: 2.4, width: 14.4),
+                  // indicator: MaterialIndicator(color:Color(0xFF000000) ,  : 2.4 , width: 14.4 ),
+                  tabs: [
+                    Tab(
+                      child: Container(
+                        child: Text('Recomemended'),
+                      ),
+                    ),
+                    Tab(
+                      child: Container(
+                        child: Text('Popular'),
+                      ),
+                    ),
+                    Tab(
+                      child: Container(
+                        child: Text('New Destination'),
+                      ),
+                    ),
+                    Tab(
+                      child: Container(
+                        child: Text('Hidden Gems'),
+                      ),
+                    )
+                  ],
+                ),
               ),
             )
           ]),
