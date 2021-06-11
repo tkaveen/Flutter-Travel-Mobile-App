b1d031ee0cb294a36cc2698856ff7a3f8542cb93
Theshan Kaveen <64361534+tkaveen@users.noreply.github.com>
2021-06-11 14:30:09 +0530

    New screen for Selected place

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:travel_app/screens/home_screen.dart';
+import 'package:travel_app/screens/selected_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
+      home: SelectedScreen(),
     );
   }
 }
