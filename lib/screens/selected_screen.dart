b3043a4f207e5449077fd9e846075efb9906a363
Theshan Kaveen <64361534+tkaveen@users.noreply.github.com>
2021-06-11 15:16:05 +0530

    update selectedscreen

--- lib/screens/selected_screen.dart
+++ lib/screens/selected_screen.dart
@@ -1,5 +1,8 @@
+import 'dart:html';
+
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 import 'package:travel_app/models/recommended_model.dart';
 
@@ -78,7 +81,7 @@ class SelectedScreen extends StatelessWidget {
                   children: [
                     SmoothPageIndicator(
                       controller: _pageController,
-                      count: recommendations.length,
+                      count: recommendedModel.images.length,
                       effect: ExpandingDotsEffect(
                           activeDotColor: Color(0xFF8a8a8a),
                           dotColor: Color(0xFFababab),
@@ -86,6 +89,73 @@ class SelectedScreen extends StatelessWidget {
                           dotWidth: 6,
                           spacing: 4.8),
                     ),
+                    Padding(
+                      padding: EdgeInsets.only(top: 19.2),
+                      child: Text(
+                        recommendedModel.tagLine,
+                        maxLines: 2,
+                        style: TextStyle(
+                            fontFamily: 'Apercu Pro',
+                            fontSize: 42.6,
+                            fontWeight: FontWeight.w700),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: 19.2),
+                      child: Text(
+                        recommendedModel.description,
+                        maxLines: 3,
+                        style: TextStyle(
+                            fontFamily: 'Apercu Pro',
+                            fontSize: 19.2,
+                            fontWeight: FontWeight.w500,
+                            color: Colors.white),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 48,
+                    ),
+                    Row(
+                      children: [
+                        Column(
+                          children: [
+                            Text(
+                              'Start from',
+                              style: GoogleFonts.lato(
+                                  fontSize: 16.8,
+                                  fontWeight: FontWeight.w500,
+                                  color: Colors.white),
+                            ),
+                            Text(
+                              '\$ ${recommendedModel.price.toString()}',
+                              style: GoogleFonts.lato(
+                                  fontSize: 21.6,
+                                  fontWeight: FontWeight.w700,
+                                  color: Colors.white),
+                            )
+                          ],
+                        ),
+                        Container(
+                          height: 62.4,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(9.6),
+                              color: Colors.white),
+                          child: Align(
+                            alignment: Alignment.center,
+                            child: Padding(
+                              padding: EdgeInsets.only(left: 28.8, right: 28.8),
+                              child: Text(
+                                'Explore Now >>',
+                                style: GoogleFonts.lato(
+                                    fontSize: 19.2,
+                                    fontWeight: FontWeight.w700,
+                                    color: Colors.black),
+                              ),
+                            ),
+                          ),
+                        )
+                      ],
+                    )
                   ],
                 ),
               ),
